2204d21753db58d055591896b0c07394b392c01d
Lucas Freund <lcs.freund@gmail.com>
2019-03-05 15:54:31 +0100

    first deploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and shitfart to die.
+            Edit <code>src/App.js</code>....
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn to commit die!
+            Learn to commit!
           </a>
         </header>
       </div>
