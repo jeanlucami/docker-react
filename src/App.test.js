88c342ea6eb621a11faae8fc54cd787744195fc3
Lucas Freund <lcs.freund@gmail.com>
2019-03-05 09:12:58 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,15 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without dabbing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without flossing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
